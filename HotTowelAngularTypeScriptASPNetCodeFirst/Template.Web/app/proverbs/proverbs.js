b0ff5afd639521b5beb7eb4d8dc0c750c64ee0e5
John Reilly <johnny_reilly@hotmail.com>
2014-07-10 15:00:03 +0100

    Converted to class.

--- HotTowelAngularTypeScriptASPNetCodeFirst/Template.Web/app/proverbs/proverbs.js
+++ HotTowelAngularTypeScriptASPNetCodeFirst/Template.Web/app/proverbs/proverbs.js
@@ -1,60 +1,116 @@
-﻿(function () {
-    'use strict';
+﻿"use strict";
+var app;
+(function (app) {
+    (function (controllers) {
+        var controllerId = "proverbs";
 
-    var controllerId = 'proverbs';
+        var Proverbs = (function () {
+            function Proverbs($q, common, datacontext) {
+                var _this = this;
+                this.$q = $q;
+                this.common = common;
+                this.datacontext = datacontext;
+                this.bySelectedSage = function (proverb) {
+                    if (!_this.selectedSage) {
+                        return true;
+                    }
+                    return proverb.user === _this.selectedSage;
+                };
+                this.proverbs = [];
+                this.sages = [];
+                this.selectedSage = undefined;
+                this.title = "Proverbs";
 
-    angular.module('app').controller(controllerId, ['$q', 'common', 'datacontext', proverbs]);
+                var getLogFn = common.logger.getLogFn;
+                this.log = getLogFn(controllerId);
 
-    function proverbs($q, common, datacontext) {
-        var getLogFn = common.logger.getLogFn;
-        var log = getLogFn(controllerId);
-
-        var vm = this;
-        vm.bySelectedSage = bySelectedSage;
-        vm.proverbs = [];
-        vm.sages = [];
-        vm.selectedSage = undefined;
-        vm.title = 'Proverbs';
-
-        activate();
-
-        function activate() {
-            var dataPromises = [getProverbs(), getSages()];
-
-            var combinerPromise = $q.all(dataPromises).then(function (eventArgs) {
-                var sageDictionary = {};
-                vm.sages.forEach(function (sage) {
-                    return sageDictionary[sage.id] = sage;
-                });
-
-                vm.proverbs.forEach(function (proverb) {
-                    return proverb.user = sageDictionary[proverb.userId];
-                });
-            });
-
-            common.activateController([combinerPromise], controllerId).then(function () {
-                return log('Activated Proverbs View');
-            });
-        }
-
-        function bySelectedSage(proverb) {
-            if (!vm.selectedSage) {
-                return true;
+                this.activate();
             }
-            return proverb.user === vm.selectedSage;
-        }
+            Proverbs.prototype.activate = function () {
+                var _this = this;
+                var dataPromises = [this.getProverbs(), this.getSages()];
 
-        function getProverbs() {
-            return datacontext.getProverbs().then(function (data) {
-                return vm.proverbs = data;
-            });
-        }
+                var combinerPromise = this.$q.all(dataPromises).then(function (eventArgs) {
+                    var sageDictionary = {};
+                    _this.sages.forEach(function (sage) {
+                        return sageDictionary[sage.id] = sage;
+                    });
 
-        function getSages() {
-            return datacontext.getSages().then(function (data) {
-                return vm.sages = data;
-            });
-        }
-    }
+                    _this.proverbs.forEach(function (proverb) {
+                        return proverb.user = sageDictionary[proverb.userId];
+                    });
+                });
+
+                this.common.activateController([combinerPromise], controllerId).then(function () {
+                    return _this.log("Activated Proverbs View");
+                });
+            };
+
+            Proverbs.prototype.getProverbs = function () {
+                var _this = this;
+                return this.datacontext.getProverbs().then(function (data) {
+                    return _this.proverbs = data;
+                });
+            };
+
+            Proverbs.prototype.getSages = function () {
+                var _this = this;
+                return this.datacontext.getSages().then(function (data) {
+                    return _this.sages = data;
+                });
+            };
+            Proverbs.$inject = ["$q", "common", "datacontext"];
+            return Proverbs;
+        })();
+
+        angular.module("app").controller(controllerId, Proverbs);
+    })(app.controllers || (app.controllers = {}));
+    var controllers = app.controllers;
+})(app || (app = {}));
+/*
+interface proverbsVm {
+bySelectedSage: (proverb) => boolean;
+proverbs: proverb[];
+selectedSage: sage;
+sages: sage[];
+title: string;
+}
+(function () {
+'use strict';
+var controllerId = 'proverbs';
+angular.module('app').controller(controllerId,
+['$q', 'common', 'datacontext', proverbs]);
+function proverbs($q: ng.IQService, common: common, datacontext: datacontext) {
+var getLogFn = common.logger.getLogFn;
+var log = getLogFn(controllerId);
+var vm: proverbsVm = this;
+vm.bySelectedSage = bySelectedSage;
+vm.proverbs = [];
+vm.sages = [];
+vm.selectedSage = undefined;
+vm.title = 'Proverbs';
+activate();
+function activate() {
+var dataPromises: ng.IPromise<any>[] = [getProverbs(), getSages()];
+var combinerPromise = $q.all(dataPromises).then((eventArgs) => {
+var sageDictionary: { [id: number]: sage } = {};
+vm.sages.forEach(sage => sageDictionary[sage.id] = sage);
+vm.proverbs.forEach(proverb => proverb.user = sageDictionary[proverb.userId]);
+});
+common.activateController([combinerPromise], controllerId)
+.then(() => log('Activated Proverbs View'));
+}
+function bySelectedSage(proverb: proverb) {
+if (!vm.selectedSage) { return true; }
+return proverb.user === vm.selectedSage
+}
+function getProverbs() {
+return datacontext.getProverbs().then(data => vm.proverbs = data);
+}
+function getSages() {
+return datacontext.getSages().then(data => vm.sages = data);
+}
+}
 })();
+*/
 //# sourceMappingURL=proverbs.js.map
--- HotTowelAngularTypeScriptASPNetCodeFirst/Template.Web/app/proverbs/proverbs.ts
+++ HotTowelAngularTypeScriptASPNetCodeFirst/Template.Web/app/proverbs/proverbs.ts
@@ -1,4 +1,68 @@
-﻿interface proverbsVm {
+﻿"use strict";
+
+module app.controllers {
+
+    var controllerId = "proverbs";
+
+    class Proverbs {
+
+        log: (message: string, data?: Object, showToast?: boolean) => void;
+        proverbs: proverb[];
+        selectedSage: sage;
+        sages: sage[];
+        title: string;
+
+        static $inject = ["$q", "common", "datacontext"];
+        constructor(
+            private $q: ng.IQService,
+            private common: common,
+            private datacontext: datacontext) {
+
+            this.proverbs = [];
+            this.sages = [];
+            this.selectedSage = undefined;
+            this.title = "Proverbs";
+
+            var getLogFn = common.logger.getLogFn;
+            this.log = getLogFn(controllerId);
+
+            this.activate();
+        }
+
+        activate() {
+            var dataPromises: ng.IPromise<any>[] = [this.getProverbs(), this.getSages()];
+
+            var combinerPromise = this.$q.all(dataPromises).then((eventArgs) => {
+
+                var sageDictionary: { [id: number]: sage } = {};
+                this.sages.forEach(sage => sageDictionary[sage.id] = sage);
+
+                this.proverbs.forEach(proverb => proverb.user = sageDictionary[proverb.userId]);
+            });
+
+            this.common.activateController([combinerPromise], controllerId)
+                .then(() => this.log("Activated Proverbs View"));
+        }
+
+        bySelectedSage = (proverb: proverb) => {
+            if (!this.selectedSage) { return true; }
+                return proverb.user === this.selectedSage
+            }
+
+        private getProverbs() {
+            return this.datacontext.getProverbs().then(data => this.proverbs = data);
+        }
+
+        private getSages() {
+            return this.datacontext.getSages().then(data => this.sages = data);
+        }
+    }
+
+    angular.module("app").controller(controllerId, Proverbs);
+}
+
+/*
+interface proverbsVm {
     bySelectedSage: (proverb) => boolean;
     proverbs: proverb[];
     selectedSage: sage;
@@ -56,3 +120,4 @@
         }
     }
 })();
+*/
